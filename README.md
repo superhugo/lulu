b18604b17ce9703b70930e85907c08ad083cef8b
Hugo Davoust <hello@hugo.io>
2015-03-25 20:58:41 +0100

    it always seems impossible until it’s done

---
+++ README.md
@@ -0,0 +1,3 @@
+# lulu
+
+Writing...
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp         = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var del          = require('del');
+var sass         = require('gulp-ruby-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var minifyCSS    = require('gulp-minify-css');
+var rename       = require('gulp-rename');
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('dist', 'watch');
+});
+
+// Concert SCSS to CSS
+gulp.task('dist', function () {
+  return sass('scss/', { sourcemap: true, style: 'expanded' })
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sourcemaps.write('maps', {
+      includeContent: false,
+      sourceRoot: '/scss'
+    }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/'));
+});
+
+// Watch for changes
+gulp.task('watch', function () {
+  gulp.watch('scss/*.scss', ['dist']);
+});
+
+// Clean dist folder
+gulp.task('clean', function () {
+  del(['dist/*']);
+});
